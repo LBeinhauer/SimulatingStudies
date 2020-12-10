d587520489c1df9e45de0a8ae859e400fd6823fd
lukas.beinhauer <lukas.beinhauer@gmail.com>
2020-12-10 16:16:39 +0100

    assessing heterogeneity

---
+++ HeterogeneityDueToVar(ES).R
@@ -0,0 +1,117 @@
+library(devtools)
+library(RCurl)
+
+source_url("https://github.com/LBeinhauer/SimulatingStudies/blob/master/SimStudies.R?raw=TRUE") # hyperlink added with ?raw=TRUE
+
+# X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=.5, mean.groupsd=2, sd.groupsd=.00000001, n.max=125, n.min=20)
+# 
+# y <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
+#          sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
+# 
+
+
+# sd.meandiff = .5
+
+pdf("simulatedHeterogeneityPlots.pdf")
+
+group.vars <- seq(from=.01, to=5.005, by=.005)^2
+
+rma.fit <- list()
+
+het.measures <- matrix(NA, nrow=length(group.vars), ncol=3)
+
+pb = txtProgressBar(min = 0, max = length(group.vars), initial = 0, style=3) 
+for(i in 1:length(group.vars)){
+  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=.5, mean.groupsd=sqrt(group.vars[i]), sd.groupsd=0, n.max=125, n.min=20)
+  
+  rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
+                      sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
+  het.measures[i,1] <- rma.fit[[i]]$I2
+  het.measures[i,2] <- rma.fit[[i]]$H2
+  het.measures[i,3] <- rma.fit[[i]]$tau2
+  setTxtProgressBar(pb,i)
+}
+
+## Apply is slower with 100 studies, than for loop (unclear why, faster with fewer, e.g. 10 studies. Likely due to est. algoirthm in rma)
+# het.measures <- apply(matrix(1:length(group.vars), ncol=1), 1, FUN = function(x) {
+#   doingit <- function(x){
+#     dat <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=.5, mean.groupsd=sqrt(group.vars[x]), sd.groupsd=0, n.max=125, n.min=20)
+#     fit <- rma(measure = "SMD", m1i = dat$study.summary$mean.X2, m2i = dat$study.summary$mean.X1, sd1i = dat$study.summary$sd.X2, 
+#         sd2i = dat$study.summary$sd.X1, n1i = dat$study.summary$N, n2i = dat$study.summary$N)
+#     return(c(fit$I2, fit$H2, fit$tau2))
+#   }
+#   doingit(x)
+# })
+
+plot(group.vars, het.measures[,1], main = "I^2 for sd.meandiff = .5")
+plot(group.vars, het.measures[,2], main = "H^2 for sd.meandiff = .5", ylim=c(0,20))
+plot(group.vars, het.measures[,3], main = "tau^2 for sd.meandiff = .5", ylim=c(0,10))
+
+
+
+# sd.meandiff=1
+
+# I^2 = tau^2 / (tau^2 = sigma^2)
+
+pb = txtProgressBar(min = 0, max = length(group.vars), initial = 0, style=3) 
+for(i in 1:length(group.vars)){
+  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=1, mean.groupsd=sqrt(group.vars[i]), sd.groupsd=.00000001, n.max=125, n.min=20)
+  
+  rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
+                      sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
+  het.measures[i,1] <- rma.fit[[i]]$I2
+  het.measures[i,2] <- rma.fit[[i]]$H2
+  het.measures[i,3] <- rma.fit[[i]]$tau2
+  setTxtProgressBar(pb,i)
+}
+
+
+plot(group.vars, het.measures[,1], main = "I^2 for sd.meandiff = 1")
+plot(group.vars, het.measures[,2], main = "H^2 for sd.meandiff = 1", ylim=c(0,20))
+plot(group.vars, het.measures[,3], main = "tau^2 for sd.meandiff = 1", ylim=c(0,10))
+
+
+# sd.meandiff=.05
+
+
+pb = txtProgressBar(min = 0, max = length(group.vars), initial = 0, style=3) 
+for(i in 1:length(group.vars)){
+  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=.05, mean.groupsd=sqrt(group.vars[i]), sd.groupsd=.00000001, n.max=125, n.min=20)
+  
+  rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
+                      sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
+  het.measures[i,1] <- rma.fit[[i]]$I2
+  het.measures[i,2] <- rma.fit[[i]]$H2
+  het.measures[i,3] <- rma.fit[[i]]$tau2
+  setTxtProgressBar(pb,i)
+}
+
+
+plot(group.vars, het.measures[,1], main = "I^2 for sd.meandiff = .05")
+plot(group.vars, het.measures[,2], main = "H^2 for sd.meandiff = .05", ylim=c(0,20))
+plot(group.vars, het.measures[,3], main = "tau^2 for sd.meandiff = .05", ylim=c(0,10))
+
+
+
+ 
+# sd.meandiff=.2
+
+
+pb = txtProgressBar(min = 0, max = length(group.vars), initial = 0, style=3) 
+for(i in 1:length(group.vars)){
+  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=.2, mean.groupsd=sqrt(group.vars[i]), sd.groupsd=.00000001, n.max=125, n.min=20)
+  
+  rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
+                      sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
+  het.measures[i,1] <- rma.fit[[i]]$I2
+  het.measures[i,2] <- rma.fit[[i]]$H2
+  het.measures[i,3] <- rma.fit[[i]]$tau2
+  setTxtProgressBar(pb,i)
+}
+
+
+plot(group.vars, het.measures[,1], main = "I^2 for sd.meandiff = .2")
+plot(group.vars, het.measures[,2], main = "H^2 for sd.meandiff = .2", ylim=c(0,20))
+plot(group.vars, het.measures[,3], main = "tau^2 for sd.meandiff = .2", ylim=c(0,10))
+
+dev.off()
--- ReadMe.md
+++ ReadMe.md
@@ -4,10 +4,12 @@
 This page contains an R-script, designed to simulate data from multiple studies. The goal is to assess meta-analytic estimates of heterogeneity, and the impact of various kinds of variance on such estimates.
 
 Currently, the following functions are contained:
-- SimStudies2groups()
+- SimStudies2groups(), in SimStudies.R
 
 ## SimStudies2groups()
 
 The following function is designed to simulate data from multiple studies, in order to assess the impact of various kinds of variances on measures of heterogeneity. Sample sizes are samples for each study, with small sample sizes being stronlgy favoured.
+
+Additionally, a second script "HeterogeneityDueToVar(ES).R" is included. This deal with the assessment of heterogeneity due to variance in the simulated Effect Sizes, in combination with a changing sampling variance. Its results, only graphical plots as of now, is found in "simulatedHeterogeneityPlots.pdf".
 
 The content of this script is subject to change, and will be developed over time.
