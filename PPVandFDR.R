03982410ecb1b23c5c59a390a7286e50d9b2b87e
lukas.beinhauer <lukas.beinhauer@gmail.com>
2020-12-16 17:13:07 +0100

    ggplot2

--- PPVandFDR.R
+++ PPVandFDR.R
@@ -8,6 +8,16 @@
 #  results. This allows the computation of PPV and False Discovery Rate (FDR)                                                   #
 #################################################################################################################################
 
+if (!require(ggplot2)) {
+  install.packages("ggplot2")
+}
+if (!require(gridExtra)) {
+  install.packages("gridExtra")
+}
+if (!require(grid)) {
+  install.packages("grid")
+}
+
 
 mFDR <- mPPV <- matrix(NA, ncol=3, nrow=1000)
 
@@ -43,15 +53,46 @@ for(ii in 1:3){
 dev.off()
 par(mfrow=c(3,2))
 
-plot(parameters[[1]]$Pb, mFDR[,1], main="FDR as function of Ratio of True Hypotheses", xlab="FDR", ylab="Ratio of True Hypotheses")
-plot(parameters[[1]]$Pb, mPPV[,1], main="PPV as function of Ratio of True Hypotheses", xlab="PPV", ylab="Ratio of True Hypotheses")
+FDR.Pb <- ggplot() +
+  geom_point(aes(x=parameters[[1]]$Pb, y=mFDR[,1]), color="darkred") +
+  labs(title="FDR as function of Ratio of True Hypotheses") +
+  xlab("Ratio of True Hypotheses") +
+  ylab("FDR") +
+  theme_gray()
+PPV.Pb <- ggplot() +
+  geom_point(aes(x=parameters[[1]]$Pb, y=mPPV[,1]), color="darkblue") +
+  labs(title="PPV as function of Ratio of True Hypotheses") +
+  xlab("Ratio of True Hypotheses") +
+  ylab("PPV") +
+  theme_gray()
 
-plot(parameters[[2]]$Power, mFDR[,2], main="FDR as function of Power", xlab="FDR", ylab="Power")
-plot(parameters[[2]]$Power, mPPV[,2], main="PPV as function of Power", xlab="PPV", ylab="Power")
+FDR.power <- ggplot() +
+  geom_point(aes(x=parameters[[2]]$Power, y=mFDR[,2]), color="darkred") +
+  labs(title="FDR as function of Power") +
+  xlab("Power") +
+  ylab("FDR") +
+  theme_gray()
+PPV.power <- ggplot() +
+  geom_point(aes(x=parameters[[2]]$Power, y=mPPV[,2]), color="darkblue") +
+  labs(title="PPV as function of Ratio of True Hypotheses") +
+  xlab("Power") +
+  ylab("PPV") +
+  theme_gray()
 
-plot(parameters[[3]]$alpha, mFDR[,3], main="FDR as function of significance level", xlab="FDR", ylab="alpha")
-plot(parameters[[3]]$alpha, mPPV[,3], main="PPV as function of significance level", xlab="PPV", ylab="alpha")
+FDR.alpha <- ggplot() +
+  geom_point(aes(x=parameters[[3]]$alpha, y=mFDR[,3]), color="darkred") +
+  labs(title="FDR as function of significance level") +
+  xlab("alpha") +
+  ylab("FDR") +
+  theme_gray()
+PPV.alpha <- ggplot() +
+  geom_point(aes(x=parameters[[3]]$alpha, y=mPPV[,3]), color="darkblue") +
+  labs(title="PPV as function of significance level") +
+  xlab("alpha") +
+  ylab("PPV") +
+  theme_gray()
 
+grid.arrange(FDR.Pb, PPV.Pb, FDR.power, PPV.power, FDR.alpha, PPV.alpha, ncol=2)
 
 
 mFDR <- mPPV <- matrix(NA, ncol=16, nrow=1000)
@@ -68,12 +109,42 @@ for(ii in 1:16){
 
 pdf(file="Power_and_PPV.pdf")
 
+
+
 par(mfrow=c(4,4))
 
-for(i in 1:ncol(mFDR)){
-  #print(plot(Power, mFDR[,i], main=sprintf("Power and FDR, at Pb=%f", Pb[i]), ylab="FDR"))
-  print(plot(Power, mPPV[,i], main=sprintf("Power and PPV, at Pb=%f", Pb[i]), ylab="PPV"))
-}
+## issue: ggplot does not work inside a loop!!!
+
+plot.list <- apply(matrix(1:ncol(mFDR), ncol=1), 1, FUN=function(x) 
+                   ggplot() +
+                     geom_point(aes(x=Power, y=mPPV[,x]), color="darkblue") +
+                     theme_gray() + 
+                     theme(axis.title = element_blank())
+                   )
+
+
+# labs(title="PPV as function of significance level") +
+#   xlab("Power") +
+#   ylab("PPV") +
+  
+# for(i in 1:ncol(mFDR)){
+#   plot.list[[i]] <- ggplot() +
+#     geom_point(aes(x=Power, y=mPPV[,i]), color="darkblue") +
+#     labs(title="PPV as function of significance level") +
+#     xlab("Power") +
+#     ylab("PPV") +
+#     theme_gray()
+#   #print(plot(Power, mFDR[,i], main=sprintf("Power and FDR, at Pb=%f", Pb[i]), ylab="FDR"))
+# }
+
+grid.arrange(plot.list[[1]], plot.list[[2]], plot.list[[3]], plot.list[[4]],
+             plot.list[[5]], plot.list[[6]], plot.list[[7]], plot.list[[8]],
+             plot.list[[9]], plot.list[[10]], plot.list[[11]], plot.list[[12]],
+             plot.list[[13]], plot.list[[14]], plot.list[[15]], plot.list[[16]], 
+             top = textGrob("PPV as function of a priori power", gp=gpar(fontsize=20, font=2)),
+             bottom = textGrob("a priori power", gp=gpar(fontsize=15, font=3)),
+             left = textGrob("PPV ", gp=gpar(fontsize=15, font=3)),
+             ncol=4)
 
 dev.off()
 
