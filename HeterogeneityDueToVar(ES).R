2b742ab1ac847033d5516e9fc9a939eac619e8be
lukas.beinhauer <lukas.beinhauer@gmail.com>
2020-12-17 18:08:40 +0100

    issue: var(ES) and tau not equivalent

--- HeterogeneityDueToVar(ES).R
+++ HeterogeneityDueToVar(ES).R
@@ -16,6 +16,9 @@ if (!require(RCurl)) {
 
 source_url("https://github.com/LBeinhauer/SimulatingStudies/blob/master/SimStudies.R?raw=TRUE") # hyperlink added with ?raw=TRUE
 
+# source("SimStudies.R")
+
+
 # X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=.5, mean.groupsd=2, sd.groupsd=.00000001, n.max=125, n.min=20)
 # 
 # y <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
@@ -25,17 +28,18 @@ source_url("https://github.com/LBeinhauer/SimulatingStudies/blob/master/SimStudi
 
 # sd.meandiff = .5
 
-pdf("simulatedHeterogeneityPlots.pdf")
+# pdf("simulatedHeterogeneityPlots.pdf")
 
 group.vars <- seq(from=.01, to=5.005, by=.005)^2
 
-rma.fit <- list()
+rma.fit <- X.data <- list()
 
 het.measures <- matrix(NA, nrow=length(group.vars), ncol=3)
 
 pb = txtProgressBar(min = 0, max = length(group.vars), initial = 0, style=3) 
-for(i in 1:length(group.vars)){
+for(i in 1:length(group.vars)){ 
   X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=.5, mean.groupsd=sqrt(group.vars[i]), sd.groupsd=0, n.max=125, n.min=20)
+  X.data[[i]] <- X
   
   rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
                       sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
@@ -44,6 +48,22 @@ for(i in 1:length(group.vars)){
   het.measures[i,3] <- rma.fit[[i]]$tau2
   setTxtProgressBar(pb,i)
 }
+
+mx <- matrix(NA, nrow=length(group.vars), ncol=5)
+
+for(i in 1:length(group.vars)){
+  mx[i,1] <- mean(X.data[[i]]$study.summary[,1])
+  mx[i,2] <- mean(X.data[[i]]$study.summary[,2])
+  mx[i,3] <- mean(X.data[[i]]$study.summary[,3])
+  mx[i,4] <- mean(X.data[[i]]$study.summary[,4])
+  mx[i,5] <- sd(X.data[[i]]$study.summary[,6])
+}
+
+df <- as.data.frame(mx)
+names(df) <- c("mean.mean.X1", "mean.mean.X2", "mean.sd.x1", "mean.sd.x2", "sd.diff")
+
+mean(df$sd.diff)
+plot(df$sd.diff)
 
 ## Apply is slower with 100 studies, than for loop (unclear why, faster with fewer, e.g. 10 studies. Likely due to est. algoirthm in rma)
 # het.measures <- apply(matrix(1:length(group.vars), ncol=1), 1, FUN = function(x) {
@@ -57,9 +77,12 @@ for(i in 1:length(group.vars)){
 # })
 
 plot(group.vars, het.measures[,1], main = "I^2 for sd.meandiff = .5")
+abline(v=.25)
+abline(h=50)
 plot(group.vars, het.measures[,2], main = "H^2 for sd.meandiff = .5", ylim=c(0,20))
-plot(group.vars, het.measures[,3], main = "tau^2 for sd.meandiff = .5", ylim=c(0,10))
-
+plot(group.vars, het.measures[,3], main = "tau^2 for sd.meandiff = .5", ylim=c(0,.3))
+abline(h=.5^2)
+abline(v=.25)
 
 
 # sd.meandiff=1
@@ -130,18 +153,19 @@ plot(group.vars, het.measures[,3], main = "tau^2 for sd.meandiff = .2", ylim=c(0
 
 
 
-
+################## varying of ES variance ######################
 
 
 ES.vars <- seq(from=.0005, to=.30, by=.0005)
 
-rma.fit <- list()
+rma.fit <- X.data <- list()
 
 het.measures <- matrix(NA, nrow=length(ES.vars), ncol=3)
 
 pb = txtProgressBar(min = 0, max = length(ES.vars), initial = 0, style=3) 
 for(i in 1:length(ES.vars)){
-  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=ES.vars[i], mean.groupsd=.2, sd.groupsd=0, n.max=125, n.min=20)
+  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=sqrt(ES.vars[i]), mean.groupsd=.2, sd.groupsd=0, n.max=125, n.min=20)
+  X.data[[i]] <- X
   
   rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
                       sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
@@ -158,13 +182,38 @@ plot(ES.vars, het.measures[,3], main = "tau^2 for mean.groupsd=.2", ylim=c(0,10)
 
 
 
+mx <- matrix(NA, nrow=length(ES.vars), ncol=5)
+
+for(i in 1:length(ES.vars)){
+  mx[i,1] <- mean(X.data[[i]]$study.summary[,1])
+  mx[i,2] <- mean(X.data[[i]]$study.summary[,2])
+  mx[i,3] <- mean(X.data[[i]]$study.summary[,3])
+  mx[i,4] <- mean(X.data[[i]]$study.summary[,4])
+  mx[i,5] <- sd(X.data[[i]]$study.summary[,6])
+}
+
+df <- as.data.frame(mx)
+names(df) <- c("mean.mean.X1", "mean.mean.X2", "mean.sd.x1", "mean.sd.x2", "sd.diff")
+
+mean(df$sd.diff^2)
+plot(df$sd.diff^2)
+plot(df$sd.diff^2, het.measures[,3])
+
+lm(het.measures[,3] ~ df$sd.diff)
+
+
+
+
+
+
+
 # mean.groupsd=.5
 
 # I^2 = tau^2 / (tau^2 = sigma^2)
 
 b = txtProgressBar(min = 0, max = length(ES.vars), initial = 0, style=3) 
 for(i in 1:length(ES.vars)){
-  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=ES.vars[i], mean.groupsd=.5, sd.groupsd=0, n.max=125, n.min=20)
+  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=sqrt(ES.vars[i]), mean.groupsd=.5, sd.groupsd=0, n.max=125, n.min=20)
   
   rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
                       sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
@@ -185,7 +234,7 @@ plot(ES.vars, het.measures[,3], main = "tau^2 for mean.groupsd=.5", ylim=c(0,10)
 
 b = txtProgressBar(min = 0, max = length(ES.vars), initial = 0, style=3) 
 for(i in 1:length(ES.vars)){
-  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=ES.vars[i], mean.groupsd=.05, sd.groupsd=0, n.max=125, n.min=20)
+  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=sqrt(ES.vars[i]), mean.groupsd=.05, sd.groupsd=0, n.max=125, n.min=20)
   
   rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
                       sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
@@ -207,7 +256,7 @@ plot(ES.vars, het.measures[,3], main = "tau^2 for mean.groupsd=.05", ylim=c(0,10
 
 b = txtProgressBar(min = 0, max = length(ES.vars), initial = 0, style=3) 
 for(i in 1:length(ES.vars)){
-  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=ES.vars[i], mean.groupsd=1, sd.groupsd=0, n.max=125, n.min=20)
+  X <- SimStudies2groups(k.studies=100, meandiff=1, intercept=2, sd.meandiff=sqrt(ES.vars[i]), mean.groupsd=1, sd.groupsd=0, n.max=125, n.min=20)
   
   rma.fit[[i]] <- rma(measure = "SMD", m1i = X$study.summary$mean.X2, m2i = X$study.summary$mean.X1, sd1i = X$study.summary$sd.X2, 
                       sd2i = X$study.summary$sd.X1, n1i = X$study.summary$N, n2i = X$study.summary$N)
